08a479f0e61656afdcfb4865980015633a60b2c0
mark <mark@dropbear.dev>
2025-05-02 12:10:43 +0000

    Clean up dev.nix file

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,10 +7,6 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.openssh # To enable signing code commits with SSH keys
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
   ];
 
   # Sets environment variables in the workspace
@@ -18,25 +14,12 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
       "Dart-Code.dart-code"
     ];
 
-    # Enable previews
+    # Disable previews
     previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
+      enable = false;
     };
 
     # Workspace lifecycle hooks
