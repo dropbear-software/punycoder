fd3531b035ce28876bfe3c9ffdf38b2e896ad571
mark <mark@dropbear.dev>
2026-02-26 13:53:41 +0000

    Update IDX to use Dart package from unstable.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,11 +2,12 @@
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "unstable"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.openssh # To enable signing code commits with SSH keys
+    pkgs.dart
   ];
 
   # Sets environment variables in the workspace
